a2e6b4a089b46bc52a9c5d2bfed75c04d1913df9
Pranav Kamath <pkama@udel.edu>
2022-12-05 12:36:06 -0500

    Added priority list

--- src/components/Lister.tsx
+++ src/components/Lister.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable no-extra-parens */
 import React from "react";
 import TileItem from "./ItemTransformer";
-import type { Tile } from "./card";
+import type { Tile, Tile2, Tile3 } from "./card";
 import { tileBedSquare } from "./data";
 import "./styles.css";
 
+const c = 0;
 const TileSequence = () => {
     return (
         <div id="furniture-list">
+            <h4>Card </h4>
+            <p>low - high</p>
             {tileBedSquare.map((f: Tile) => (
                 <div key={f.name}>
-                    <p className="furniture-label">
-                        {f.name.charAt(0).toUpperCase() + f.name.slice(1)}
-                    </p>
+                    {f.name + f.priority}
                     <TileItem item={f} />
                 </div>
             ))}
---
+++ src/components/mCard.ts
@@ -0,0 +1,14 @@
+export interface medBed {
+    id: string;
+    name: "-";
+    priority: 2;
+    description: string;
+    bunk: boolean;
+    height: number;
+    width: number;
+    top: number;
+    left: number;
+    color: string;
+}
+
+export type Tile2 = medBed;
