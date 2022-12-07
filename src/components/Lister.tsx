34d653af08b1df35f0c84bf5c5a59733d7f38df6
Pranav Kamath <pkama@udel.edu>
2022-12-07 13:24:36 -0500

    all

--- src/components/Lister.tsx
+++ src/components/Lister.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-extra-parens */
 import React from "react";
+import { RevealAnswer } from "./RevealAnswer";
 import "./styles.css";
 const TileSequence = () => {
     return (
         <div id="furniture-lit">
             <h4>Task:</h4>
+            <RevealAnswer></RevealAnswer>
         </div>
     );
 };
