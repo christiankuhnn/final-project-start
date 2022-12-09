71313563d1ed20733365b1510ed1b80ef9e98908
Pranav Kamath <pkama@udel.edu>
2022-12-08 19:33:42 -0500

    Now you can save the card title property through modal form

--- src/components/ItemTransformer.tsx
+++ src/components/ItemTransformer.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-extra-parens */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState, ReactDOM } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Form, Modal } from "react-bootstrap";
 import { useDrag } from "react-dnd";
+import { setTokenSourceMapRange } from "typescript";
 import type { Tile } from "./card";
 
 interface ItemSet {
@@ -11,10 +12,25 @@ interface ItemSet {
 }
 
 const TileItem = ({ item, deleteTile }: ItemSet) => {
-    const { id, name, left, top, height, width, color } = item;
+    const { id, left, top, height, width, color } = item;
     const [position, setPosition] = useState({ top: top, left: left });
     const [isHovered, setIsHovered] = useState(false);
-    const [isFilter, setIsFilter] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+    const [title, setModTitle] = useState(item.id);
+    const [desc, setModalDescription] = useState("");
+    const [name, setName] = useState("Empty");
+
+    const titleHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setModTitle(event.target.value);
+    };
+    const descHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setModalDescription(event.target.value);
+    };
+    const cardPropHandler = () => {
+        setName(title);
+    };
 
     const [{ isDragging }, drag] = useDrag({
         item: {
@@ -43,6 +59,7 @@ const TileItem = ({ item, deleteTile }: ItemSet) => {
     };
 
     const showDimensionsAndIcon = isHovered && !id.includes("menu");
+
     useEffect(() => {
         const elem = document.getElementById(item.id)?.getBoundingClientRect();
         const t = elem ? elem.y : 0;
@@ -57,12 +74,41 @@ const TileItem = ({ item, deleteTile }: ItemSet) => {
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-            <div style={{ width: "100%", height: "100%" }}>
+            <div style={{ width: "10%", height: "100%" }}>
                 {showDimensionsAndIcon && (
-                    <>
-                        <Button>View Task</Button>
-                    </>
+                    <button onClick={handleShowModal}>{name}</button>
                 )}
+                <Modal show={showModal} onHide={handleCloseModal}>
+                    <Modal.Header>
+                        <Modal.Title>Edit {name}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Col>
+                            <Form.Group className="makeNoteTitle">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control
+                                    type="textarea"
+                                    placeholder="Task title..."
+                                    value={title}
+                                    onChange={titleHandler}
+                                    autoFocus
+                                />
+                                <p></p>
+                                <Form.Label>Desc</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="I need to..."
+                                    value={desc}
+                                    onChange={descHandler}
+                                    autoFocus
+                                ></Form.Control>
+                                <p></p>
+
+                                <Button onClick={cardPropHandler}>Save</Button>
+                            </Form.Group>
+                        </Col>
+                    </Modal.Body>
+                </Modal>
                 {showDimensionsAndIcon && (
                     <p
                         id="delete-button"
